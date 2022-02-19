eadbfad9d662c8eb37ce9841a006bd3088363a8c
M N Tushar <mntushar25@gmail.com>
2022-02-19 17:11:00 +0600

    add department model

---
+++ StudentManagementSystem/Migrations/202202191105140_updateDeparmentModel.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateDeparmentModel : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.DepartmentModels", "DeptName", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.DepartmentModels", "DeptName", c => c.String());
+        }
+    }
+}
---
+++ StudentManagementSystem/Models/DepartmentModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models
+{
+    public class DepartmentModels
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string DeptName { get; set; }
+        public virtual List<StudentModel> Students { get; set; }
+    }
+}
