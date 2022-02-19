8c4c00307144884704c36d7f9909ef24127eecb6
M N Tushar <mntushar25@gmail.com>
2022-02-19 17:09:14 +0600

    setup database

---
+++ StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models.ModelContext
+{
+    public class UniversityDBContext : DbContext
+    {
+        public DbSet<DepartmentModels> Department { get; set; }
+        public DbSet<StudentModel> Student { get; set; }
+    }
+}
