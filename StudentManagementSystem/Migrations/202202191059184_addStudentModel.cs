fa977118a4d539de44dff7b3e57d19062e4678e9
M N Tushar <mntushar25@gmail.com>
2022-02-19 17:12:02 +0600

    add student model

---
+++ StudentManagementSystem/Migrations/202202191059184_addStudentModel.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addStudentModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.StudentModels",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        DateOfBirth = c.DateTime(nullable: false),
+                        DeptId = c.Int(nullable: false),
+                        Department_Id = c.Int(),
+                    })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.DepartmentModels", t => t.Department_Id)
+                .Index(t => t.Department_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.StudentModels", "Department_Id", "dbo.DepartmentModels");
+            DropIndex("dbo.StudentModels", new[] { "Department_Id" });
+            DropTable("dbo.StudentModels");
+        }
+    }
+}
---
+++ StudentManagementSystem/Models/StudentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models
+{
+    public class StudentModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
+        public int DeptId { get; set; }
+
+        public virtual DepartmentModels Department { get; set; }
+    }
+}
