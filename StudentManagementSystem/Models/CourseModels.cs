05dbc8156b725c14a2e9ae1e66282e66ee65389d
M N Tushar <mntushar25@gmail.com>
2022-02-21 00:37:27 +0600

    add course model

---
+++ StudentManagementSystem/Models/CourseModels.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models
+{
+    public class CourseModels
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public int SeatCount { get; set; }
+
+        [Required]
+        public int Fee { get; set; }
+
+
+        //navigration proprty
+        public virtual List<StudentRegistrationModels> StudentRegistration { get; set; }
+    }
+}
