901a3467286242385b2186ae07949e2aec6d048c
M N Tushar <mntushar25@gmail.com>
2022-02-21 00:36:56 +0600

    db context migration Fluent  api

--- StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
+++ StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Web;
 
 namespace StudentManagementSystem.Models.ModelContext
@@ -10,5 +11,15 @@ namespace StudentManagementSystem.Models.ModelContext
     {
         public DbSet<DepartmentModels> Department { get; set; }
         public DbSet<StudentModel> Student { get; set; }
+        public DbSet<CourseModels> Course { get; set; }
+        public  DbSet<StudentRegistrationModels> StudentRegistration { get; set; }
+
+
+        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        //{
+        //    modelBuilder.Entity<StudentModel>()
+        //        .HasOptional(a => a.StudentRegistration)
+        //        .WithRequired(ab => ab.Student);
+        //}
     }
 }
