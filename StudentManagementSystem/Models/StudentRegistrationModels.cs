616f1c40b41855b5ee7ab115890f625e98c1c8a2
M N Tushar <mntushar25@gmail.com>
2022-02-21 00:37:51 +0600

    add student registration model

---
+++ StudentManagementSystem/Models/StudentRegistrationModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Models
+{
+    public class StudentRegistrationModels
+    {
+        [Key, ForeignKey("Student")]
+        public int StudentId { get; set; }
+
+        [Required]
+        [ForeignKey("Course")]
+        public int CourseId { get; set; }
+
+        [Required]
+        public DateTime EnrollDate { get; set; }
+        public bool IsPaymentComplete { get; set; }
+
+
+        //navigration proprty
+        public virtual StudentModel Student { get; set; }
+        public virtual CourseModels Course { get; set; }
+        
+    }
+}
