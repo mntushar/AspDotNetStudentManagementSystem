b94631a80cacbbeb204c1a840cc4c2e1786bddc6
M N Tushar <mntushar25@gmail.com>
2022-02-22 17:08:03 +0600

    set error cotrol

---
+++ StudentManagementSystem/Controllers/ErrorController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementSystem.Controllers
+{
+    public class ErrorController
+    {
+        public string DataSaveCustomError()
+        {
+            return "You enter worng information..";
+        }
+    }
+}
