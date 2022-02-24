12c8589ef84429cea1f97fd8757ed7ff9d8f4ead
M N Tushar <mntushar25@gmail.com>
2022-02-24 19:33:07 +0600

    remove ApplicationDbContext form universitubdcontext

--- StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
+++ StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
@@ -49,17 +49,4 @@ namespace StudentManagementSystem.Models.ModelContext
             return userIdentity;
         }
     }
-
-    //public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
-    //{
-    //    public ApplicationDbContext()
-    //        : base("DefaultConnection", throwIfV1Schema: false)
-    //    {
-    //    }
-
-    //    public static ApplicationDbContext Create()
-    //    {
-    //        return new ApplicationDbContext();
-    //    }
-    //}
 }
