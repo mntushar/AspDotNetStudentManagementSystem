184a5794c2f1cf2e9bb2b0cd4e5847f8ecd5a8df
M N Tushar <mntushar25@gmail.com>
2022-02-24 21:38:34 +0600

    remove authintication setup form main branch

--- StudentManagementSystem/Controllers/ManageController.cs
+++ StudentManagementSystem/Controllers/ManageController.cs
@@ -32,9 +32,9 @@ namespace StudentManagementSystem.Controllers
             {
                 return _signInManager ?? HttpContext.GetOwinContext().Get<ApplicationSignInManager>();
             }
-            private set 
-            { 
-                _signInManager = value; 
+            private set
+            {
+                _signInManager = value;
             }
         }
 
@@ -333,7 +333,7 @@ namespace StudentManagementSystem.Controllers
             base.Dispose(disposing);
         }
 
-#region Helpers
+        #region Helpers
         // Used for XSRF protection when adding external logins
         private const string XsrfKey = "XsrfId";
 
@@ -384,6 +384,6 @@ namespace StudentManagementSystem.Controllers
             Error
         }
 
-#endregion
+        #endregion
     }
 }
--- StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
+++ StudentManagementSystem/Models/ModelContext/UniversityDBContext .cs
@@ -1,9 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity;
+﻿using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
 using System.Linq;
 using System.Reflection.Emit;
 using System.Web;
+using System.Data.Entity;
 
 namespace StudentManagementSystem.Models.ModelContext
 {
@@ -12,14 +13,6 @@ namespace StudentManagementSystem.Models.ModelContext
         public DbSet<DepartmentModels> Department { get; set; }
         public DbSet<StudentModel> Student { get; set; }
         public DbSet<CourseModels> Course { get; set; }
-        public  DbSet<StudentRegistrationModels> StudentRegistration { get; set; }
-
-
-        //protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        //{
-        //    modelBuilder.Entity<StudentModel>()
-        //        .HasOptional(a => a.StudentRegistration)
-        //        .WithRequired(ab => ab.Student);
-        //}
+        public DbSet<StudentRegistrationModels> StudentRegistration { get; set; }
     }
 }
