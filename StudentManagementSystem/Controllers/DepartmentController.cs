ee0aa130f931befb263184c1217b03aa3f066425
M N Tushar <mntushar25@gmail.com>
2022-02-26 02:42:18 +0600

    rm unnecessary comment

--- StudentManagementSystem/Controllers/DepartmentController.cs
+++ StudentManagementSystem/Controllers/DepartmentController.cs
@@ -51,7 +51,6 @@ namespace StudentManagementSystem.Controllers
 
         // POST: DepartmentModels/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Create([Bind(Include = "Id,DeptName")] DepartmentModels department)
@@ -90,7 +89,6 @@ namespace StudentManagementSystem.Controllers
 
         // POST: DepartmentModels/Edit/5
         // To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult Edit([Bind(Include = "Id,DeptName")] DepartmentModels department)
